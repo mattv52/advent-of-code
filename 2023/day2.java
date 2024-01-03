34cd8b764a30e3fd345a37851595ed3a640f798d
mattv52 <matthewvolaris@yahoo.com.au>
2024-01-03 11:56:29 +1030

    Finished day 2

--- 2023/day2.java
+++ 2023/day2.java
@@ -4,27 +4,36 @@ public class day2 extends AOC {
     public static void main(String[] args) throws Exception {
         ArrayList<String> input = read(2);
         int sum = 0;
-        int MAX_RED = 12;
-        int MAX_GREEN = 13;
-        int MAX_BLUE = 14;
+        // PART 1
+        // int MAX_RED = 12;
+        // int MAX_GREEN = 13;
+        // int MAX_BLUE = 14;
 
         for (String line : input) {
-            String game = line.split("Game |:")[1];
+            // String game = line.split("Game |:")[1];
             String[] pulls = line.split(": ")[1].split("; ");
-            Boolean valid = true;
+            // Boolean valid = true;
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+            
             for (String pull : pulls) {
                 for (String color : pull.split(", ")) {
                     int num = Integer.parseInt(color.split(" ")[0]);
                     String c = color.split(" ")[1];
+                    
                     switch (c) {
                         case "red":
-                            if (num > MAX_RED) valid = false;
+                            if (num > red) red = num;
+                            // if (num > MAX_RED) valid = false;
                             break;
-                        case "green":
-                            if (num > MAX_GREEN) valid = false;
+                            case "green":
+                            if (num > green) green = num;
+                            // if (num > MAX_GREEN) valid = false;
                             break;
-                        case "blue":
-                            if (num > MAX_BLUE) valid = false;
+                            case "blue":
+                            if (num > blue) blue = num;
+                            // if (num > MAX_BLUE) valid = false;
                             break;
                     
                         default:
@@ -32,8 +41,8 @@ public class day2 extends AOC {
                     }
                 }
             }
-            if (valid) sum += Integer.parseInt(game);
-            // System.out.println(pulls[1]);
+            // if (valid) sum += Integer.parseInt(game);
+            sum += red*green*blue;
         }
         System.out.println(sum);
     }
